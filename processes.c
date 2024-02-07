152cba4d15b42f496ad90715a1a6a9ebec569ab5
Sebastian Sunga <sebastian.sunga834@my.csun.edu>
2024-02-06 20:46:27 -0800

    goofy user input implementation

--- processes.c
+++ processes.c
@@ -8,21 +8,21 @@
 
 typedef struct Process{
     
-    int process_id;
-    struct Process * parent_process;
-    struct Process * child_process;
-    struct Process * younger_sib;
-    struct Process * older_sib;
+    int ProcessID;
+    struct Process * ParentPCB;
+    struct Process * ChildPCB;
+    struct Process * SiblingPCB;
+
 } process;
 
 
-process * list[max_process_count]; //<-- array of processes, which can be linked by a linked list?
+process * PcbArray[max_process_count]; //<-- array of processes, which can be linked by a linked PcbArray?
                  // Actuallly can we just link them with pointers? Because  
                  // Try younger sibling, older sibling
 
-void allocate_list(){
+void allocate_PcbArray(){
     for(int i; i < max_process_count ; i ++){
-        list[i] = malloc(sizeof(process));
+        PcbArray[i] = malloc(sizeof(process));
         
     }
 }
@@ -32,70 +32,70 @@ void allocate_list(){
 //  1: running
 //  -1: blocked
 
-void command_create(int id){
+void CreatePCB(int PCBid){
 	// First ever process
 	// Creation of a parent process, no children
-	list[id] = malloc(sizeof(process));
+	process * PCB = malloc(sizeof(process));
 	
-	list[id]->process_id = id;
-	list[id]->parent_process = NULL;
-	list[id]->child_process = NULL;
-	
-	list[id]->younger_sib = NULL;
-	list[id]->older_sib = NULL;
-	
+    PCB->ProcessID = PCBid;
+    PCB->ParentPCB = NULL; // Points to nothing
+    PCB->ChildPCB = NULL; // points to nothing at initilization
+    PCB->SiblingPCB = NULL; //Has no Siblings
+ 
+    PcbArray[PCBid] = PCB;	
 	
 };
 
 
-void create_child_process( int id, int parent_id){
-     list[id] = malloc(sizeof(process));
+void CreateChildPCB( int PCBid, int ParentPcbId ){
+     process * childPCB = malloc(sizeof(process));
      
-     list[id]->process_id = id;
-     
-     
-     list[id]->parent_process = list[parent_id];
-     
-      list[id]->younger_sib = NULL;
-      list[id]->older_sib = NULL;
-     
-     if(list[parent_id]->child_process != NULL){
-       process * temptr = list[parent_id]->child_process;
-       while(temptr->younger_sib != NULL){
-           temptr = temptr->younger_sib;
-       }
-       
-       temptr->younger_sib = list[id];
-      
-      list[id]->older_sib = temptr;
-      list[id]->younger_sib = NULL;
-      
-     
-     }
-       else
-     list[parent_id]->child_process = list[id];
-     
-     
-     
+     childPCB->ProcessID = PCBid;
+     childPCB->ParentPCB = PcbArray[ParentPcbId];
+ 
+     process * PCBSearch = PcbArray[ParentPcbId];
 
-    // list[id] = new_proc;
-     
+     if(PCBSearch->ChildPCB == NULL){ // Parent process has no inital child, start of linked list
+       
+       PCBSearch->ChildPCB = childPCB;
+       childPCB->SiblingPCB = NULL; // No siblings yet
+       childPCB->ChildPCB = NULL;
+     }
+     else{
+             PCBSearch = PCBSearch->ChildPCB;
+       while(PCBSearch->SiblingPCB != NULL){
+          PCBSearch = PCBSearch->SiblingPCB; // Go through linked list
+
+       }
+          PCBSearch->SiblingPCB = childPCB;
+          childPCB->SiblingPCB = NULL;
+          childPCB->ChildPCB = NULL;
+         // Found end of linked list
+       
+     }
+    
+     PcbArray[PCBid] = childPCB; 
 };  // <--- 
 
 
-process * destroy_process(int p_id){ 
+int DestroyPCB(int PCBid){ 
         // If process has no children or siblings at actually
-        if(list[p_id]->child_process == NULL && list[p_id]->younger_sib == NULL){
-           list[p_id] = NULL;
+         process * Moron = PcbArray[PCBid];
+        if(Moron->ChildPCB == NULL && Moron->SiblingPCB == NULL){
+           PcbArray[PCBid] = NULL;
+           free(Moron);
+
+           
+
        
 
-           return free(list[p_id]);
+           return 1;
         }
-        else if(list[p_id]->younger_sib != NULL){
-           return destroy_process(list[p_id]->younger_sib->process_id);
+        else if(Moron->SiblingPCB != NULL){
+           return DestroyPCB(Moron->SiblingPCB->ProcessID);
         }
 	 else {
-	    return destroy_process(list[p_id]->child_process->process_id);	
+	    return DestroyPCB(Moron->ChildPCB->ProcessID);	
 	 }
     
 }; // <--- Recursive function
@@ -103,19 +103,22 @@ process * destroy_process(int p_id){
 
 void print_proccesses(){
 	for(int i = 0; i < max_process_count ; i ++){
-	    if(list[i] == NULL){
-	       printf("Slot #%d is free.\n", i);// Do nothing
+	    if(PcbArray[i] == NULL){
+	      // printf("Slot #%d is free.\n", i);// Do nothing
 	    }
 	    else{
-	    process * tempr = list[i];
-	    printf("Process ID [%d]\n", tempr->process_id);
+	    process * TempPCBpointer = PcbArray[i];
+	    printf("Process ID [%d]\n", TempPCBpointer->ProcessID);
+        printf("In Memory: %p\n", TempPCBpointer);
 	    
-	    if(tempr->child_process != NULL){
+        process * ChildTEMPpointer = TempPCBpointer->ChildPCB;
+	    if(ChildTEMPpointer != NULL){
 	    printf("Child Processes ->");
-	       tempr = tempr->child_process;
-	      while(tempr != NULL){
-	        printf(" [%d] ",tempr->process_id);
-	        tempr = tempr->younger_sib;
+	       TempPCBpointer = TempPCBpointer->ChildPCB;
+	      while(TempPCBpointer != NULL){
+	        printf(" [%d] ",TempPCBpointer->ProcessID);
+            printf("In Memory: %p\n", TempPCBpointer );
+	        TempPCBpointer = TempPCBpointer->SiblingPCB;
 	      }
 	      printf("\n");
 	      
@@ -128,7 +131,7 @@ void print_proccesses(){
 	}
 	 
     printf("====================\n");
-    	//printf("\n Procces's children %d \n", list[0]->child_process->process_id); // This will be xpanded with a for loop to go through children actuallyl children to null, younger sibling to null
+    	//printf("\n Procces's children %d \n", PcbArray[0]->child_process->ProcessID); // This will be xpanded with a for loop to go through children actuallyl children to null, younger sibling to null
 };
 
 
@@ -139,24 +142,7 @@ int main(int argc, char *argv[]){
       int proc_parent;
       int process_num_init;
        int Process_To_Destroy;
-     // allocate_list();
-
-    /*   Test functions
-      command_create(0);
-      create_child_process(1,0);
-      create_child_process(2,0);
-      create_child_process(5,1);
-      create_child_process(10,2);
-      create_child_process(4,0);
-      command_create(6);
-      create_child_process(9,6);
-      print_proccesses();
-      destroy_process(0);
-      print_proccesses(); */ 
-     // destroy_process(6); Create a check if's a parent I'm deleting that has no parent of there own beforehand
-      
-     
-
+    
      while(user_argument != 4){
      print_proccesses();
      printf("\n Test program for abstraction of process creation, please enter in a numerical argument 1-5 \n");
@@ -172,18 +158,18 @@ int main(int argc, char *argv[]){
           	       printf("Initilize process Hierarchy \n");
           	       printf("Enter in process id: ");
           	       scanf("%d", &process_num_init);
-          	       command_create(process_num_init);
+          	       CreatePCB(process_num_init);
           	       break;
           case 2:
           	       printf("Create\n");
-          	       if(list[0] == NULL)
+          	       if(PcbArray[0] == NULL)
           	       	printf(" DNE!");
           	       else{
           	       	 printf("Enter in id for process: ");
           	       	 scanf("%d",&process_num);
           	       	 printf("\nEnter parent of process %d: ",process_num);
           	       	 scanf("%d",&proc_parent);
-          	       	 create_child_process(process_num, proc_parent);
+          	       	 CreateChildPCB(process_num, proc_parent);
           	       }
 
           	       break;
@@ -191,8 +177,8 @@ int main(int argc, char *argv[]){
           	    
           	      printf("Enter in Process to destroy \n");
           	      scanf("%d",&Process_To_Destroy);
-          	      destroy_process(Process_To_Destroy);
-          	      printf("%d",list[Process_To_Destroy]);
+          	      DestroyPCB(Process_To_Destroy);
+          	      
           	      break;
           case 4:
           	      printf("Exiting ");
@@ -214,7 +200,7 @@ int main(int argc, char *argv[]){
 // IE 1 is child of 0
 //   Make 2 a child of 0
 // go through 0 -> child til 0->child is null 
-// Then 0->child->younger_sib is null
+// Then 0->child->SiblingPCB is null
 // when that is null, make 0->child->younger point to 2, whose 2->older_sibling is 0->child, 
 // Check if we need to pass in the siblings as arguments, because we just go through the the children, then siblings with a temporary pointer
    // Goal of this is to do some thing 0 , 1 ,2 with 1 and 2 as children of 0
